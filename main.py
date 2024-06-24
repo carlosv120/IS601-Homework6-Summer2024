16d441ff4bc3060c49f40b01e815e65c8fa6104e
carlosv120 <carlosv.12044@gmail.com>
2024-06-24 02:43:00 -0400

    Fixing main

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import sys
-import os
-
-# Fixing while debugging
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from calculator import Calculator
 
 if __name__ == "__main__":
